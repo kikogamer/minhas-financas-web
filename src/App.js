22319456e15716c1b738dcecebfc10c64203160a
Ronaldo <kikogamer@gmail.com>
2020-10-16 14:28:44 -0300

    #13 - spinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import LoadingContext from './contexts/LoadingContext';
+import Loading from './components/Loading';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  const showLoading = (newMessage) => {
+    setLoading(true);
+    setMessage(newMessage);
+  };
+
+  const hideLoading = () => setLoading(false);
+
+  const value = {
+    loading,
+    message,
+    showLoading,
+    hideLoading,
+  };
+
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <LoadingContext.Provider value={value}>
+      <BrowserRouter>
+        <Routes />
+        <Loading loading={loading} message={message} />
+      </BrowserRouter>
+    </LoadingContext.Provider>
   );
 }
 
