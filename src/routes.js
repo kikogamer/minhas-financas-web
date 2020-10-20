8e0e38e5709e08d8438115912d9a0693f9eb4752
Ronaldo <kikogamer@gmail.com>
2020-10-20 12:15:24 -0300

    #16 - 404 page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Page404 from './pages/404';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 
@@ -7,6 +8,7 @@ const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/cadastro" exact component={SignUp} />
+    <Route path="*" component={Page404} />
   </Switch>
 );
 
