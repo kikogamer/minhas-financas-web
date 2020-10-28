be2b9f0fd4eef052eec777b33397ac7e6962bdfe
Ronaldo <kikogamer@gmail.com>
2020-10-28 08:33:49 -0300

    #5 - sign-in page

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Page404 from './pages/404';
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/cadastro" exact component={SignUp} />
+    <Route path="/entrar" exact component={SignIn} />
     <Route path="*" component={Page404} />
   </Switch>
 );
