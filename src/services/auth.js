40e278e31f07e3c1f9e39d2e39a8011d022aca40
Ronaldo <kikogamer@gmail.com>
2020-10-30 19:33:32 -0300

    #27 - show authenticated user name

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,6 @@
 const TOKEN_KEY = '@minhas-financas-Token';
 
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const login = (token) => localStorage.setItem(TOKEN_KEY, token);
 export const logout = () => localStorage.removeItem(TOKEN_KEY);
