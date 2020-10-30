9363ce2b29f234843d77fcb85b73b71bda5ead63
Ronaldo <kikogamer@gmail.com>
2020-10-30 08:39:08 -0300

    #6 - logout option

---
+++ src/components/DropDown/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const DropDownContainer = styled.label`
+  display: inline-block;
+  margin-right: 20px;
+  position: relative;
+`;
+
+export default DropDownContainer;
